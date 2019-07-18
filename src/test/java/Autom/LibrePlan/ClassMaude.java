931ad1af3ddf77507e73754e20de11fe557f4508
maudelajoinie <maudelajoinie@gmail.com>
2019-07-18 18:48:04 +0200

    acutualisation_maude

--- src/test/java/Autom/LibrePlan/ClassMaude.java
+++ src/test/java/Autom/LibrePlan/ClassMaude.java
@@ -1,18 +1,23 @@
 package Autom.LibrePlan;
 
 import org.junit.Before;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class Class3 {
+public class ClassMaude extends PageMenu{
 	String nav = "chrome";
+	String url = "http://localhost:8090/libreplan/";
+	WebDriver driver;
 	
 	@Before
 	public void setUp() {
 		Outil.choisirUnNavigateur(nav);
+		driver.get(url);
 	}
 	
 	// ACTION Aller à la page des participants
-	public PageParticipants ressources_particpants() {
+	public PageParticipants ressources_participants() {
+		ressources.click();
 		  return PageFactory.initElements(driver, PageParticipants.class);
 
 	}
