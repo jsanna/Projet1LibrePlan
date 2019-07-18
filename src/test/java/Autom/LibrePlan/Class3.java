182f7c932c632ce49e6409f5272de8e9dc0f5dc0
maudelajoinie <maudelajoinie@gmail.com>
2019-07-18 18:25:27 +0200

    maude

--- src/test/java/Autom/LibrePlan/Class3.java
+++ src/test/java/Autom/LibrePlan/Class3.java
@@ -1,5 +1,19 @@
 package Autom.LibrePlan;
 
-public class Class3 {
+import org.junit.Before;
+import org.openqa.selenium.support.PageFactory;
 
+public class Class3 {
+	String nav = "chrome";
+	
+	@Before
+	public void setUp() {
+		Outil.choisirUnNavigateur(nav);
+	}
+	
+	// ACTION Aller à la page des participants
+	public PageParticipants ressources_particpants() {
+		  return PageFactory.initElements(driver, PageParticipants.class);
+
+	}
 }
