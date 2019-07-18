baa31ff4c27b6aeb0782eea2dc927bc62ec37983
unknown <daria.kantsedal@gmail.com>
2019-07-18 18:25:41 +0200

    login

--- src/main/java/Autom/LibrePlan/PageIndex.java
+++ src/main/java/Autom/LibrePlan/PageIndex.java
@@ -1,5 +1,6 @@
 package Autom.LibrePlan;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -7,14 +8,19 @@ import org.openqa.selenium.support.PageFactory;
 public class PageIndex {
 
 	@FindBy (xpath = "//input[@name = 'j_username']")
-	WebElement utilisateur;
+	private static WebElement utilisateur;
 	
 	@FindBy (xpath = "//input[@name = 'j_password']")
-	WebElement mot_de_pass;
+	private static WebElement mot_de_pass;
+	
+	@FindBy (xpath = "//input[@name = 'button']")
+	private static WebElement submit;
 	
 	public static PageMenu login(WebDriver driver)
 	
 	{
+		Outil.renseignerChamp(utilisateur, "admin");
+		
 		return PageFactory.initElements(driver, PageMenu.class)
 	}
 	
