5d2c159e1445987927913f587e1a95a6082230f8
maudelajoinie <maudelajoinie@gmail.com>
2019-07-18 17:38:26 +0200

    ajout Maude.

--- src/main/java/Autom/LibrePlan/PageMenu.java
+++ src/main/java/Autom/LibrePlan/PageMenu.java
@@ -1,24 +1,26 @@
 package Autom.LibrePlan;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class PageMenu {
 	
-	
+	@FindBy (xpath = "//button[contains(.,'Calendrier')]")
 	protected WebElement calendrier;
 	
+	@FindBy (xpath = "//button[contains(.,'Ressources')]")
 	protected WebElement ressources;
-	
+	@FindBy (xpath = "//button[contains(.,'Coût')]")
 	protected WebElement cout;
-	
+	@FindBy (xpath = "//button[contains(.,'Configuration')]")
 	protected WebElement configuration;
-	
+	@FindBy (xpath = "//button[contains(.,'Communication')]")
 	protected WebElement communication;
-	
+	@FindBy (xpath = "")
 	protected WebElement creer_nouveau_projet;
-	
+	@FindBy (xpath = "")
 	protected WebElement rapport;
-	
+	@FindBy (xpath = "")
 	protected WebElement impression;
 	
 	
