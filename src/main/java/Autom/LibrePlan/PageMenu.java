8ccd471a74c4aea11e29dd3648ab8ab2c6ae9276
formation <daria.kantsedal@gmail.com>
2019-07-18 16:50:55 +0200

    pagemenu1

---
+++ src/main/java/Autom/LibrePlan/PageMenu.java
@@ -0,0 +1,25 @@
+package Autom.LibrePlan;
+
+import org.openqa.selenium.WebElement;
+
+public class PageMenu {
+	
+	
+	protected WebElement calendrier;
+	
+	protected WebElement ressources;
+	
+	protected WebElement cout;
+	
+	protected WebElement configuration;
+	
+	protected WebElement communication;
+	
+	protected WebElement creer_nouveau_projet;
+	
+	protected WebElement rapport;
+	
+	protected WebElement impression;
+	
+	
+}
