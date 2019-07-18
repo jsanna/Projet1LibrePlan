e7ce4d2f50bfbb7ef75be320de771dfc55212cfa
unknown <jsanna@qualixo.com>
2019-07-18 13:50:22 +0200

    Dossier de base

---
+++ src/main/java/Autom/LibrePlan/App.java
@@ -0,0 +1,13 @@
+package Autom.LibrePlan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
