0add401349adf411046e85a7ce5b454821878d9b
formation <daria.kantsedal@gmail.com>
2019-07-19 14:45:52 +0200

    test daria avec tableau

--- src/test/java/Autom/LibrePlan/TestDaria.java
+++ src/test/java/Autom/LibrePlan/TestDaria.java
@@ -3,30 +3,29 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
+
+
 public class TestDaria extends TestAbstract {
+	List<WebElement> tableau;
+	
+	@Test
 	public void tester()
 	{
 		PageIndex index = PageFactory.initElements(driver, PageIndex.class);
 		assertTrue("[FAIL pas de bouton calendrier]",index.calendrier.isDisplayed());
 		
-		/*
-			public void clickSurRessourcesPuisParticipants (WebDriver driver)
-	{
-		Actions a = new Actions(driver);
-		//Peut etre il faut une pause Thread.sleep
-		a.moveToElement(ressources).build().perform();
-		//Peut etre il faut une pause Thread.sleep
-		a.moveToElement(ressources)
-				.moveToElement(driver.findElement(By.xpath("//a[@href = '/libreplan/qualityforms/qualityForms.zul']"))).click()
-				.build().perform();
-	}  
-*/
+//		Passer la souris sur l'onglet "Ressources" puis dans le sous-menu qui s'affiche, cliquer sur l'item "Formulaires de qualité".
+		index.clickMenu(driver, "Ressources", "Formulaires qualité");
 		
-	//	Passer la souris sur l'onglet "Ressources" puis dans le sous-menu qui s'affiche, cliquer sur l'item "Formulaires de qualité".
 		
 	/*	Affichage de la page "Formulaires qualité Liste".
 
@@ -45,6 +44,19 @@ public class TestDaria extends TestAbstract {
 
 		- un bouton [Créer]
 		*/
+		
+		assertEquals("FAIL page n'est pas Formulaire de qualité", "LibrePlan: formulaires qualité",driver.getTitle());
+		// Tableau Xpath //tr[substring(@id,5)='t4']
+		tableau = driver.findElements(By.xpath("//tr[substring(@id,5)='t4']/th/div"));
+		
+		assertFalse(tableau.isEmpty());
+		assertEquals("FAIL tableau n'a pas le meme quantite de colonnes", 3, tableau.size());
+		Outil.verificationTextWebElement("Nom", tableau.get(0));
+		Outil.verificationTextWebElement("Description", tableau.get(1));
+		Outil.verificationTextWebElement("Opérations", tableau.get(2));
+		
+		assertTrue(driver.findElement(By.className("create-button global-action z-button")).isDisplayed());
+		
 		
 		
 	}
