bc21b81d6bf096b2f682eec10c20293eafdfcba7
formation <daria.kantsedal@gmail.com>
2019-07-19 13:27:03 +0200

    pagemenu methode de Amos

--- src/main/java/Autom/LibrePlan/PageMenu.java
+++ src/main/java/Autom/LibrePlan/PageMenu.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class PageMenu {
 
@@ -52,6 +54,20 @@ public abstract class PageMenu {
 //				.moveToElement(driver.findElement(By.xpath("//div/a[substring(@id,4)='Qt-a']"))).click()
 //				.build().perform();
 //		return PageFactory.initElements(driver, PageParticipants.class);
+	}
+	
+	public void clickMenu(WebDriver driver, String title, String subtitle) {
+		WebElement section = driver.findElement(By.xpath("//*[contains(@class,'mainmenu')]/descendant::button[contains(.,'"+ title.trim()+"')]/.."));
+		
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(section);
+		actions.build().perform();
+		
+		Actions actions2 = new Actions(driver);
+		actions2.moveToElement(wait.until(ExpectedConditions.visibilityOf(section.findElement(By.xpath("//div[contains(@class, 'submenu')]/ul/li/descendant::a[contains(., '"+subtitle+"')]")))));
+		actions2.click();
+		actions2.build().perform();
 	}
 	
 	
