5fda5872d2196e0dd12b9f0f2c78b23257d8fe42
maudelajoinie <maudelajoinie@gmail.com>
2019-07-19 14:49:12 +0200

    Get it

--- src/test/java/Autom/LibrePlan/ClassTestMaude.java
+++ src/test/java/Autom/LibrePlan/ClassTestMaude.java
@@ -1,8 +1,9 @@
 package Autom.LibrePlan;
 
-import org.junit.Before;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 public class ClassTestMaude extends TestAbstract{
@@ -12,7 +13,15 @@ public class ClassTestMaude extends TestAbstract{
 	// ACTION Connexion
 	PageIndex page_index = PageFactory.initElements(driver, PageIndex.class);
 	
+	// VERIFICATION onglet calendrier présent
+	assertEquals("Calendrier ", driver.findElement(By.xpath("//td/div/button[substring(@id,5)='7-b']")).getText());
+	
 	// ACTION Direction vers page Participants	
 	page_index.clickMenu(driver, "Ressources", "Participants");
+	
+	// VERIFICATION page Participants
+	assertEquals("LibrePlan: Participants", driver.getTitle());
+		//verif tableau à faire
+	assertTrue(driver.findElement(By.xpath("//input[substring(@id,4)='Qq4-real']")).isDisplayed());
 	}
 }
