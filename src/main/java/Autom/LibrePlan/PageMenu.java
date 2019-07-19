98e7f287a37b2146284388a05ab48c95547a49bd
formation <daria.kantsedal@gmail.com>
2019-07-19 10:40:14 +0200

    PageMenu est abstract

--- src/main/java/Autom/LibrePlan/PageMenu.java
+++ src/main/java/Autom/LibrePlan/PageMenu.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
-public class PageMenu {
+public abstract class PageMenu {
 
 	@FindBy(xpath = "//button[contains(.,'Calendrier')]")
 	protected WebElement calendrier;
