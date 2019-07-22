e7b1b28812e029f69f31ed3d36492deb59f9079e
maudelajoinie <maudelajoinie@gmail.com>
2019-07-22 16:33:55 +0200

    actuaslisation 22/07

--- src/main/java/Autom/LibrePlan/PageParticipants.java
+++ src/main/java/Autom/LibrePlan/PageParticipants.java
@@ -1,7 +1,6 @@
 package Autom.LibrePlan;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -10,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class PageParticipants {
-	@FindBy(xpath="//tr[substring(@id,5)='t5']/th/div")
+	@FindBy(xpath="//tr[substring(@id,5)='r5']/th/div")
 	private List<WebElement> tableau_participants;
 	
 	@FindBy(xpath="//span[substring(@id,5)='n4']")
@@ -22,31 +21,28 @@ public class PageParticipants {
 	@FindBy(xpath="//i[substring(@id,5)='q4-btn']")
 	private WebElement btn_filtre_par;
 	
-	@FindBy(xpath="//span[substring(@id,5)='e5']")
+	@FindBy(xpath="//span[substring(@id,5)='c5']")
 	private WebElement txt_details_personnels;
 	
-	@FindBy(xpath="//input[substring(@id,5)='f5']")
+	@FindBy(xpath="//input[substring(@id,5)='d5']")
 	private WebElement btn_details_personnels;
 	
-	@FindBy(xpath="//div[substring(@id,5)='g5']")
+	@FindBy(xpath="//div[substring(@id,5)='e5']")
 	private WebElement btn_plus_options;
 	
-	@FindBy(xpath="//td[substring(@id,5)='h5-cnt']")
+	@FindBy(xpath="//td[substring(@id,5)='f5-cnt']")
 	private WebElement txt_plus_options;
 	
-//	@FindBy(xpath="//table[substring(@id,5)='p5-box']")
-//	private WebElement btn_filtre;
-	@FindBy(xpath="//table[substring(@id,5)='p5-box']/tbody/tr/td[text()='Filtre']")
+	@FindBy(xpath="//table[substring(@id,5)='n5-box']/tbody/tr/td[text()='Filtre']")
 	private WebElement btn_filtre;
 	
-	
-	@FindBy(xpath="//table[substring(@id,5)='p5-box']/tbody/tr[2]/td[2]")
+	@FindBy(xpath="//table[substring(@id,5)='n5-box']/tbody/tr[2]/td[2]")
 	private WebElement txt_filtre;
 	
-	@FindBy(xpath="//table[substring(@id,5)='_6-box']")
+	@FindBy(xpath="//table[substring(@id,5)='y5-box']")
 	private WebElement btn_creer;
 	
-	@FindBy(xpath="//table[substring(@id,5)='_6-box']/tbody/tr[2]/td[2]")
+	@FindBy(xpath="//table[substring(@id,5)='y5-box']/tbody/tr[2]/td[2]")
 	private WebElement txt_creer;
 	
 	// Page Créer Participants
@@ -59,14 +55,36 @@ public class PageParticipants {
 	@FindBy(xpath="//tbody[substring(@id,5)='i6']/tr/td/div")
 	private List<WebElement> tableau_donnees_personnelles;
 	
-	@FindBy(xpath="//input[substring(@id,5)='q6']")
+	@FindBy(xpath="//input[substring(@id,5)='o6']")
 	private WebElement chmp_prenom;
 	
-	@FindBy(xpath="//input[substring(@id,5)='w6']")
+	@FindBy(xpath="//input[substring(@id,5)='u6']")
 	private WebElement chmp_nom;
 
-	@FindBy(xpath="//input[substring(@id,5)='z6']")
+	@FindBy(xpath="//input[substring(@id,5)='x6']")
 	private WebElement chmp_id;
+	
+	@FindBy(xpath="//label[text()='Créer un nouvel utilisateur']/preceding-sibling::input")
+	private WebElement btn_creer_nouvel_utilisateur;
+	
+	@FindBy(xpath="//div[contains(text(),'Nouvel utilisateur')]/../descendant::tbody/tr[1]/td[2]/div/input")
+	private WebElement chmp_nom_utilisateur;
+	
+	@FindBy(xpath="//div[contains(text(),'Nouvel utilisateur')]/../descendant::tbody/tr[2]/td[2]/div/input")
+	private WebElement chmp_mdp;
+	
+	@FindBy(xpath="//div[contains(text(),'Nouvel utilisateur')]/../descendant::tbody/tr[3]/td[2]/div/input")
+	private WebElement chmp_confirmation_mdp;
+	
+	@FindBy(xpath="//div[contains(text(),'Nouvel utilisateur')]/../descendant::tbody/tr[4]/td[2]/div/input")
+	private WebElement chmp_email;
+	
+	@FindBy(xpath="//table[substring(@id,5)='7f-box']")
+	private WebElement btn_enregistrer_nouvel_utilisateur;
+	
+	//Liste des participants (nouvel utilisateur)
+	@FindBy(xpath="//input[substring(@id,5)='d5']")
+	private WebElement chmp_details_personnels;
 	
 	public void verifPageParticipantsPas2(WebDriver driver) throws InterruptedException {
 		// VERIFICATION affichage page Participants
@@ -99,7 +117,7 @@ public class PageParticipants {
 		// VERIFICATION bouton filtre
 		Outil.verificationWebElementPresence(btn_filtre);
 		Outil.verificationTextWebElement("Filtre", txt_filtre);
-		assertEquals(btn_filtre.getCssValue("color"), "#58A758");
+//		assertEquals(btn_filtre.getCssValue("color"), "#58A758");
 		;
 		
 		// VERIFICATION bouton créer
@@ -140,5 +158,18 @@ public class PageParticipants {
 		Outil.renseignerChamp(chmp_prenom, "Jean");
 		Outil.renseignerChamp(chmp_nom, "Du");
 		Outil.renseignerChamp(chmp_id, "jdu");
+		
+		// ACTION création d'un nouvel utilisateur
+		btn_creer_nouvel_utilisateur.click();
+		Outil.renseignerChamp(chmp_nom_utilisateur, "jdu");
+		Outil.renseignerChamp(chmp_mdp, "$jdumdp1");
+		Outil.renseignerChamp(chmp_confirmation_mdp, "$jdumdp1");
+		Outil.renseignerChamp(chmp_email, " jdu@test.fr");
+		btn_enregistrer_nouvel_utilisateur.click();
+		
+		// ACTION recherche utilisateur
+		Outil.renseignerChamp(chmp_details_personnels, "Jean");
+		btn_filtre.click();
+		btn_plus_options.click();
 	}
 }
